77f921b6d44b133bed79c58396d74eee9dcd06bd
Jeremy Poulin <jaypoulz@gmail.com>
2025-12-23 12:19:35 -0500

    fix: add explicit pull check for default fedora container
    The build instructions allow you to specify a Fedora version,
    but default to the 'latest' tag. For those of use who haven't updated
    the fedora:latest image in years,
    this can result in the build defaulting to Fedora 36.
    Claude requires nodejs newer than 16.

--- build-claude.sh
+++ build-claude.sh
@@ -8,4 +8,7 @@ if [ "$1" = "--rebuild" ]; then
     shift
 fi
 
+# Ensure default base image is up-to-date
+podman pull --policy=newer registry.fedoraproject.org/fedora:latest
+
 podman build -t claude:latest --build-arg UID="$(id -ru)" --build-arg GID="$(id -rg)" "${build_args[@]}" "$(dirname "${0}")"
